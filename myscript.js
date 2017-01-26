d9aa982299c22982eca7fbf674fbef87d8145642
danielyoun2004 <danielyoun2004@gmail.com>
2017-01-25 23:14:22 -0500

    addpages

---
+++ myscript.js
@@ -0,0 +1,21 @@
+var operand1;
+var operand2;
+
+function getNumbes(){
+  operand1 = Math.floor(Math.random() * 50) +1;
+  operand2 = Math.floor(Math.random() * 50) +1;
+  document.getElementById("operand1").innerHTML = operand1;
+  document.getElementById("operand2").innerHTML = operand2;
+  document.getElementById("userAnswer").innerHTML ="";
+} 
+
+function checkAnswer(){
+  var userSum = document .getElementById("userAnswer").value;
+  var sum = operand1 + operand2;
+  if(userSum == sum){
+    document.getElementById("results").innerHTML="Correct!";
+  }
+  else{
+    document.getElementById("results").innerHTML= "Wrong!";
+  }
+}
