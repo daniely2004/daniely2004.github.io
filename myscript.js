3bdd9b7a969e72290cc278493b137bba3c70a3fd
danielyoun2004 <danielyoun2004@gmail.com>
2017-02-01 20:01:04 -0500

    addpages

--- myscript.js
+++ myscript.js
@@ -20,25 +20,4 @@ function checkAnswer(){
   }
 }
 
-var operand1;
-var operand2;
-
-function getNumbers(){
-  operand1 = Math.floor(Math.random() * 50) -1;
-  operand2 = Math.floor(Math.random() * 50) -1;
-  document.getElementById("operand1").innerHTML = operand1;
-  document.getElementById("operand2").innerHTML = operand2;
-  document.getElementById("userAnswer").innerHTML ="";
-} 
-
-function checkAnswer(){
-  var userDifference = document .getElementById("userAnswer").value;
-  var differece = operand1 - operand2;
-  if(userDifferece == difference){
-    document.getElementById("results").innerHTML="Correct!";
-  }
-  else{
-    document.getElementById("results").innerHTML= "Wrong!";
-  }
-}
 
