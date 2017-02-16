bfe070a16681b315f337473eb2663a74adca3d96
danielyoun2004 <danielyoun2004@gmail.com>
2017-02-15 22:51:05 -0500

    addpages

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 var character = "";
 
 function chooseX(){
-  character = 'x';
+  character = 'X';
 }
 
 function chooseO(){
@@ -22,10 +22,37 @@ function makeMove(divLocation){
   
   if(checkWinner()){
     document.getElementById("notes").innerHTML=character +" wins!";
+    alert(character +"won! Lets play again!");
+    location.reload();
   }
 }
 
-
+function checkWinner(){
+  var r1c1 = document.getElementById("row1col1").innerHTML;
+  var r1c2 = document.getElementById("row1col2").innerHTML;
+  var r1c3 = document.getElementById("row1col3").innerHTML;
+  var r2c1 = document.getElementById("row2col1").innerHTML;
+  var r2c2 = document.getElementById("row2col2").innerHTML;
+  var r2c3 = document.getElementById("row2col3").innerHTML;
+  var r3c1 = document.getElementById("row3col1").innerHTML;
+  var r3c2 = document.getElementById("row3col2").innerHTML;
+  var r3c3 = document.getElementById("row3col3").innerHTML;
+  
+ if(r1c1 === r1c2 && r1c1 === r1c3 && r1c1 !== ""||
+    r2c1 === r2c2 && r2c1 === r2c3 && r2c1 !== ""||
+    r3c1 === r3c2 && r3c1 === r3c3 && r3c1 !== ""|| 
+    r1c1 === r2c1 && r1c1 === r1c1 && r1c1 !== ""||
+    r1c2 === r2c2 && r2c2 === r2c2 && r2c2 !== ""||
+    r1c3 === r2c3 && r3c3 === r3c3 && r3c3 !== ""||
+    r1c1 === r2c2 && r1c1 === r3c3 && r1c1 !== ""||
+    r1c3 === r2c2 && r1c3 === r3c1 && r1c3 !== ""){
+        return true;
+  }
+  else{
+    return false;
+  } 
+  
+}
 
 
 var operand1;
